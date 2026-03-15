e47853970486512f04bb2482504032a5188a1ccc
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2026-03-14 22:52:58 -0700

    fixes

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -27,9 +27,9 @@
   }
 </script>
 
-<div class="max-w-5xl p-1 mx-auto flex">
+<div class="max-w-5xl p-1 mx-auto flex mt-20">
   <form
-    class="flex flex-col gap-3 w-1/2 text-white"
+    class="flex flex-col gap-4 w-1/2 text-white mx-auto items-center"
     onsubmit={(e) => { e.preventDefault(); onSubmit(); }}
   >
     <h2 class="text-4xl font-bold ">Contact Me</h2>
@@ -37,17 +37,17 @@
       Leave your email and a message below and il get back to you! Or find me on
       any of my socials!
     </p>
-    <div class="rounded-xl border border-neutral-500/50 flex flex-col">
+    <div class="rounded-xl border border-neutral-500/50 flex flex-col w-full">
       <input
         required
         bind:value={email}
         type="email"
         placeholder="your email address..."
-        class="text-neutral-400 bg-transparent rounded-t-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-neutral-700/50 placeholder:text-neutral-700"
+        class="text-neutral-400 bg-transparent rounded-t-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-neutral-700/50 placeholder:text-neutral-700 w-full"
       />
       <textarea
         required
-        class="text-neutral-400 bg-transparent border-t rounded-b-xl border-neutral-500/50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-neutral-700/50 placeholder:text-neutral-700"
+        class="text-neutral-400 bg-transparent border-t rounded-b-xl border-neutral-500/50 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-neutral-700/50 placeholder:text-neutral-700 w-full"
         bind:value={message}
         placeholder="something nice hopefully..."
         rows="5"
